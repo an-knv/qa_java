0593c9a6279a39461fcb917fa8b7f34a169c826b
Anna Konovalova <anknv@yandex.ru>
2025-11-27 16:05:36 +0300

    проект по 5 спринту

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,9 +1,6 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
--- src/test/java/com/example/LionParameterizedTest.java
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -1,13 +1,10 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 class LionParameterizedTest {
 
